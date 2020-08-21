1dd75b372077cecb3236f5149844bc4c3590b8bf
danielchowyc <daniellchow@gmail.com>
2020-08-21 19:31:49 +0100

    can display image

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ import apiKey from './config.js';
 
 const img = document.querySelector('img');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=foDeMsQyPkE4t1hd1XcfUutg14uRHLoo&s=kobe', {mode: 'cors'})
+fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=kobe`, {mode: 'cors'})
     .then(function(response) {
-      console.log(response.json());
-    });
-
+      return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch(function() {
+      alert("No Gif could be found");
+    })
+   
 
 
