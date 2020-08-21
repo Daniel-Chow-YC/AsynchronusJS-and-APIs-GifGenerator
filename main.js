e7f06f65b7521d0dab907c440ddb483ea29a0f37
danielchowyc <daniellchow@gmail.com>
2020-08-21 21:43:41 +0100

    can use enter button on search bar

--- main.js
+++ main.js
@@ -2,18 +2,6 @@ import apiKey from './config.js';
 
 const img = document.querySelector('img');
 
-/* Using Promises */ 
-// fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=kobe`, {mode: 'cors'})
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(response) {
-//       img.src = response.data.images.original.url;
-//     })
-//     .catch(function() {
-//       alert("No Gif could be found");
-//     })
-
 
 /* Using async and await */
 async function getGif() {
@@ -26,10 +14,34 @@ async function getGif() {
   }
 };
 
+/* Using Promises */ 
+// fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=kobe`, {mode: 'cors'})
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(response) {
+//       img.src = response.data.images.original.url;
+//     })
+//     .catch(function() {
+//       alert("No Gif could be found");
+//     })
+
 window.onload = getGif();
 
 const searchbar = document.getElementById("searchbar");
 const button = document.querySelector("button");
+
+
+// Execute a function when the user releases the enter key on the keyboard
+searchbar.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    button.click();
+  }
+}); 
 
 button.addEventListener("click", getNewGif);
 
